e33623e9cd394bf76b1d5bb9cd8a663ded4dc515
felixDev22 <felixnzomo@gmail.com>
2022-12-06 18:49:52 +0300

    Add modules folder. Add variables.js, Luxon, displayBooks, DateDisplay, addEvent

---
+++ Modules/variables.js
@@ -0,0 +1,5 @@
+// Export to DisplayBooks
+export const bookContainer = document.querySelector('.books-container');
+export const bookTitle = document.querySelector('#title');
+export const bookAuthor = document.querySelector('#author');
+export const addBtn = document.querySelector('.add-btn');
