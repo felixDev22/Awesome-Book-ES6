fceea8721f4f0ea0db276e1c4aa4ec86c7618319
felixDev22 <felixnzomo@gmail.com>
2022-12-07 10:44:19 +0300

    Update modules

--- Modules/dateDisplay.js
+++
@@ -1,7 +0,0 @@
-import { dateEl } from './variableList.js';
-import { DateTime } from './luxon.js';
-
-export default () => {
-  const now = DateTime.now();
-  dateEl.innerHTML = now.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
-};
